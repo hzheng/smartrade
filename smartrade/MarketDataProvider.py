9370510fa52141ca82f8568d0953a08d017a0616
Hui Zheng <xyz.dll@gmail.com>
2022-03-20 09:50:49 -0400

    fix quote date bug

--- smartrade/MarketDataProvider.py
+++ smartrade/MarketDataProvider.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from calendar import weekday
-from datetime import date, datetime, timedelta
+from datetime import datetime, time, timedelta, timezone
 
 from dateutil.parser import parse
 import pymongo
@@ -12,6 +11,9 @@ from smartrade.BrokerClient import BrokerClient
 logger = app_logger.get_logger(__name__)
 
 class MarketDataProvider:
+    OPEN_TIME = time(13, 30)
+    CLOSE_TIME = time(20)
+
     def __init__(self, broker: BrokerClient, db_name: str):
         self._broker = broker
         mongo_client = pymongo.MongoClient()
@@ -21,31 +23,59 @@ class MarketDataProvider:
     @classmethod
     def _trading_day(cls, day):
         weekday = day.weekday()
-        if 1 <= weekday <= 5: return day
+        if 0 <= weekday < 5: return day
 
-        return day - timedelta((weekday + 2) % 7)
+        return day - timedelta((weekday + 3) % 7)
+
+    @classmethod
+    def _day_range(cls, day, min_time=None, max_time=None):
+        min_date = datetime.combine(day.date(), min_time or datetime.min.time())
+        max_date = datetime.combine(day.date(), max_time or datetime.max.time())
+        return {'date': {'$gte': min_date, '$lte': max_date}}
+
+    def _get_quotes(self, symbols, day, min_time=None, max_time=None):
+        return self._quote_collection.find({**self._day_range(day, min_time, max_time),
+                                            'symbol': {'$in': symbols}}).limit(1)
 
     def get_quotes(self, symbols, day=None):
-        today = self._trading_day(datetime.combine(date.today(), datetime.min.time()))
-        if not day:
-            day = today
-        elif isinstance(day, str):
-            day = parse(day)
-        logger.debug("get quotes for %s at the day: %s", symbols, day)
-        day = self._trading_day(day)
-        has_latest_quote = True
-        if day >= today:
-            has_latest_quote = self._retrieve_and_save(symbols, today)
+        '''
+        day: date in UTC timezone, default: today's date
+        '''
         if isinstance(symbols, str):
             symbols = [symbols]
         elif not isinstance(symbols, list):
             symbols = list(symbols)
-        if has_latest_quote:
-            res = self._quote_collection.find({'date': day, 'symbol': {'$in': symbols}})
+
+        time_zone = timezone.utc
+        now = datetime.now(time_zone)
+        current_trading_time = self._trading_day(now)
+        market_open_time = datetime.combine(current_trading_time, self.OPEN_TIME).replace(tzinfo=time_zone)
+        market_close_time = datetime.combine(current_trading_time, self.CLOSE_TIME).replace(tzinfo=time_zone)
+        logger.debug("open time: %s, close time: %s", market_open_time, market_close_time)
+        if not day:
+            day = now
+        elif isinstance(day, str):
+            day = parse(day)
+        logger.debug("get quotes for %s at the day: %s", symbols, day)
+        res = None
+        if market_open_time <= day <= market_close_time:
+            logger.debug("market is open.")
+        else:
+            day = datetime.combine(self._trading_day(day).date(), self.CLOSE_TIME)
+            logger.debug("market is closed, check if we already have record for day: %s", day)
+            res = [doc for doc in self._get_quotes(symbols, day, self.CLOSE_TIME)]
+            if len(res) == len(symbols):
+                logger.debug("we already have all data")
+            else:
+                logger.debug("we don't have all data")
+                res = None
+        if not res and self._retrieve_and_save(symbols, day):
+            res = self._get_quotes(symbols, day)
+        if res:
             return {doc['symbol']: ((doc['bidPrice'] + doc['askPrice']) / 2,
-                                    doc['markChangeInDouble'],
+                                    doc['netChange'], #doc['markChangeInDouble'],
                                     doc['netPercentChangeInDouble']) for doc in res}
-        
+                
         logger.warning("resort to old quote for %s", symbols)
         quotes = {}
         for symbol in symbols:
@@ -54,12 +84,13 @@ class MarketDataProvider:
                 quotes[symbol]=(doc['bidPrice'] + doc['askPrice']) / 2
         return quotes
 
-    def _retrieve_and_save(self, symbols, today):
+    def _retrieve_and_save(self, symbols, day):
+        logger.debug("retrieving quote at date %s...", day)
         try:
             quotes = self._broker.get_quotes(symbols)
             for symbol, quote in quotes.items():
-                specifier = {'symbol': symbol, 'date': today}
-                q = {**specifier}
+                specifier = {**self._day_range(day), 'symbol': symbol}
+                q = {'symbol': symbol, 'date': day}
                 for key in ('bidPrice', 'askPrice', 'lastPrice', 'openPrice',
                             'highPrice', 'lowPrice', 'closePrice', 'netChange',
                             'markPercentChangeInDouble', 'markChangeInDouble',
